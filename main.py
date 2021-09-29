e4671edd1e5f10f7a1584e826e8e50e9492aafa8
Dagem Tadesse <dagem.seyfu@gmail.com>
2021-09-29 14:02:04 +0300

    final touch

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from prime_artihmetic import genPrime
 
 
 def main():
-    q = genPrime(3, 5)
-    p = genPrime(q + 1, q + 6)
+
+    message = "A"
+
+    numbered = ""
+
+    for char in message:
+        numbered += "{}".format(ord(char))
+    numbered = int(numbered)
+
+    q = genPrime(numbered, numbered + 100)
+    p = genPrime(q + 1, q + 100)
 
     print("p = {}, q = {}".format(p, q))
 
@@ -17,10 +26,18 @@ def main():
 
     print("public Key = {}\nprivate Key = {}".format(pubKey, privKey))
 
-    message = 12
 
-    encrypted = encrypt(message, pubKey, n)
-    decrypted = decrypt(encrypted, privKey, n)
+    encrypted = encrypt(numbered, pubKey, n)
+
+    
+    decryptedNum = decrypt(encrypted, privKey, n)
+    decrypted = ""
+
+
+    while decryptedNum > 0:
+        decrypted += chr(int(decryptedNum % 100))
+        decryptedNum /= 100
+
 
     print("encrypted = {0}\ndecrypted = {1}".format(encrypted, decrypted))
 
